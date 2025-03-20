93cf0b3804853d279fcb3c5812e24ce264678859
andreamaluta <andreamaluta@gmail.com>
2025-03-20 14:14:24 +0100

    [UPD] created the variable for nuts exercise

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ESERCIZIO DEI DADI
+
+// creo le variabili che contengono i valori numerici
+// variabile per il computer
+let computerNum = Math.floor((Math.random()*6)+1);
+console.log(computerNum);
+// variabile per l'utente
+let userNum = Math.floor((Math.random()*6)+1);
+console.log(userNum);
